4604174d2a646819554df0c74037af38db6823c3
Harry Blades <htsblades@gmail.com>
2022-09-22 23:34:04 +0200

    Added chart component with props to call exhange rates API with proper parameters

--- ccy-viz-app/src/charting/chart.tsx
+++ ccy-viz-app/src/charting/chart.tsx
@@ -1,21 +1,79 @@
 import Plot from 'react-plotly.js';
 import * as React from 'react';
+import { useEffect, useState } from 'react';
 
-function Chart() {
-  console.log("I'm here");
+export interface IProps {
+  startDate: string;
+  endDate: string;
+  baseCcy: string;
+  targetCcy: string;
+}
+
+function Chart({ baseCcy, targetCcy, startDate, endDate }: IProps) {
+  const myHeaders = new Headers();
+  myHeaders.append('apikey', 'OnHP4Wo5ZHrQGktL4s0SGEvTSauIL3nj');
+  const [xs, setXs] = useState([]);
+  const [ys, setYs] = useState([]);
+
+  const requestOptions = {
+    method: 'GET',
+    headers: myHeaders
+  };
+
+  async function fetchExchangeApiData() {
+    let exchangeApiData;
+    try {
+      const response = await fetch(
+        `https://api.apilayer.com/exchangerates_data/timeseries?start_date=${startDate}&end_date=${endDate}&base=${baseCcy}&symbols=${targetCcy}`,
+        requestOptions
+      );
+      exchangeApiData = await response.json();
+    } catch (err) {
+      console.log('Could not retrieve currencies');
+    }
+    return exchangeApiData;
+  }
+
+  useEffect(() => {
+    let active = true;
+    const load = async () => {
+      const retrievedData = await fetchExchangeApiData();
+
+      console.log(Object.values(retrievedData.rates));
+
+      const xs = Object.keys(retrievedData.rates);
+      const ys = Object.values(retrievedData.rates).map((data) => Object.values(data)[0]);
+      if (!active) {
+        return;
+      }
+      setXs(xs);
+      setYs(ys);
+    };
+
+    load();
+    return () => {
+      active = false;
+    };
+  }, [startDate, endDate, baseCcy, targetCcy]);
+
   return (
     <Plot
       data={[
         {
-          x: [1, 2, 3],
-          y: [2, 6, 3],
+          x: xs,
+          y: ys,
           type: 'scatter',
           mode: 'lines+markers',
           marker: { color: 'red' }
-        },
-        { type: 'bar', x: [1, 2, 3], y: [2, 5, 3] }
+        }
       ]}
-      layout={{ width: 320, height: 240, title: 'A Fancy Plot' }}
+      layout={{
+        title: `Base Currency: ${baseCcy}`,
+        autosize: true,
+        xaxis: { title: 'Time' },
+        yaxis: { title: `${targetCcy}` }
+      }}
+      useResizeHandler={true}
     />
   );
 }
